a605d97c7883b327ac407a66db78d464c2df5fb2
Oisin OK <oisin66@gmail.com>
2024-12-22 00:19:19 +0000

    Changes to baseline variables comparison - all on one page, refined variable lists, improved table appearance, dynamic title

--- DIG_app/app.R
+++ DIG_app/app.R
@@ -12,6 +12,8 @@ library(tidyverse)
 library(table1)
 library(plotly)
 library(DT)
+library(survival)
+library(survminer)
 
 #Data Cleaning
 #load in data
@@ -65,7 +67,7 @@ mortality.df <- mutate(mortality.df, Monthly_Risk = Deaths/Enrolled)
 
 # Variable Lists
 
-# Categorical Variables
+# Variables Categories
 baseline_vars <- c("TRTMT", "AGE", "SEX", "BMI", "KLEVEL", "CREAT", "DIABP", "SYSBP", "HYPERTEN", "CVD", "WHF", "DIG", "HOSP")
 cat_vars <- c("TRTMT", "SEX", "HYPERTEN", "CVD", "WHF", "DIG", "HOSP", "DEATH")
 cont_vars <- c("AGE", "BMI", "KLEVEL", "CREAT", "DIABP", "SYSBP", "HOSPDAYS", "DEATHDAY")
@@ -116,26 +118,16 @@ ui <- fluidPage(
                         
                         #sidebar - input control, select a variable to see its effect on mortality
                         sidebarPanel(
-                          selectInput("base_var1", "Choose a variable to compare baseline values:", choices = baseline_vars, selected = "AGE"),
-                          selectInput("base_var2", "Choose a variable to compare across:", choices = cat_vars) # make this optional? so if nothing selected, single box plot summarising variable
+                          selectInput("base_var1", "Choose a continuous variable to compare baseline values:", choices = cont_vars, selected = "AGE"),
+                          selectInput("base_var2", "Choose a condition to compare across:", choices = cat_vars) # make this optional? so if nothing selected, single box plot summarising variable
                         ),
                         ###
                         
                         #main panel - outputs
                         mainPanel(
-                                  tabsetPanel(
-                                              tabPanel("Summary Statistics",
-                                                       uiOutput("table_baseline_compare")
-                                                       
-                                              ), # tab1 close
-                                              
-                                              tabPanel("Visualistion",
-                                                       
-                                                       plotlyOutput("boxplot_baseline_compare_plotly")
-                                                       
-                                              ) # tab2 close
-                                  ) # tabset close
-                          
+                                   uiOutput("baseline_table_title"),
+                                   uiOutput("table_baseline_compare"),
+                                   plotlyOutput("boxplot_baseline_compare_plotly")
                         ) # main close
                         ###
                         
@@ -230,7 +222,7 @@ ui <- fluidPage(
 )# UI close
 
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   ####################### SERVER CODE TAB 1 ##################################################################################################
   
@@ -239,27 +231,57 @@ server <- function(input, output) {
   
   ####################### SERVER CODE TAB 2 ##################################################################################################
   
-  # Baseline Variables Outputs
-  reactive({
+  # selected variables
+  bvar_1 <- reactive({input$base_var1})
+  bvar_2 <- reactive({input$base_var2})
+  
+  #update bvar_2 depending on bvar_1 choice
+  filtered_base_var2_choices <- reactive({
+    setdiff(cat_vars, input$base_var1) # Remove selected `base_var1` from `cat_vars`
+  })
+  
+  observeEvent(input$base_var1, {
+    updateSelectInput(
+      session, 
+      "base_var2",
+      choices = filtered_base_var2_choices(),
+      selected = NULL # Reset selection
+    )
+  })
+  
+  
+  
+  # Table1 Dynamic Title
+  output$baseline_table_title <- renderUI({
+    req(input$base_var1, input$base_var2)
     
+    # Create a dynamic title
+    title_text <- paste("Comparison of", bvar_1(), "by", bvar_2())
+    
+    # Render the title as an HTML element
+    tags$h3(title_text, style = "margin-bottom: 20px;")
   })
   
   # BASELINE COMPARISONS - TAB 2  
   
   output$table_baseline_compare <- renderUI({
-    var1 <- input$base_var1
-    var2 <- input$base_var2
+    req(input$base_var1, input$base_var2) #prevent incompatible comparisons
     
-    formula <- as.formula(paste("~", input$base_var1, "|", input$base_var2))
+    #remove NA values from base variables
+    clean_data.df <- dig.df %>%
+      filter(!is.na(.[[bvar_1()]]) & !is.na(.[[bvar_2()]]))
     
-    table1(formula, data = dig.df) # generate table1 with two input variables # maybe make this able to select multiple variables in rows later?
+    formula <- as.formula(paste("~", bvar_1(), "|", bvar_2()))
+    
+    tbl <- table1(formula, data = clean_data.df) # generate table1 with two input variables # maybe make this able to select multiple variables in rows later?
+    
+    tbl_df <- as.data.frame(tbl) #convert to dataframe to make it prettier
+    
+    DT::datatable(tbl_df, rownames = FALSE, options = list(paging = FALSE, dom = 't', autoWidth = TRUE)) %>%
+      DT::formatStyle(columns = 1, fontWeight = "bold" # Make row names bold
+      )
   })
 
-  
-
-  # selected variables
-  bvar_1 <- reactive({input$base_var1})
-  bvar_2 <- reactive({input$base_var2})
   
   # box plots
   output$boxplot_baseline_compare_plotly <- renderPlotly({
@@ -274,7 +296,6 @@ server <- function(input, output) {
         colors = c("red", "blue")
       ) %>%
       layout(
-        title = paste("Comparison of", bvar_1(), "across", bvar_2()),
         xaxis = list(title = bvar_2()),
         yaxis = list(title = bvar_1())
       )
